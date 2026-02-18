780fcd2c4993fcab410accbb1e7bcaa9ea732bbf
Hanzel Legarda <v-hlegarda+expedia@expediagroup.com>
2026-02-18 13:42:29 -0600

    feat: finished task 8

--- task/8-task-stop.py
+++ task/8-task-stop.py
@@ -1,22 +1,25 @@
-from task.app.main import run
+from task.app.client import DialClient
+from task.app.main import DIAL_ENDPOINT, DEFAULT_SYSTEM_PROMPT
+from task.models.conversation import Conversation
+from task.models.message import Message
+from task.models.role import Role
 
-# TODO:
-#  Try `stop` parameter.
-#  `stop` (str or list[str]): Tells the AI to stop generating text when it encounters specific words or phrases.
-#  Like setting custom "end of response" triggers.
-#       Default: None
-#  User massage: Explain the key components of a Large Language Model architecture
 
-run(
-    deployment_name='gpt-4o',
-    print_only_content=True,
-    # TODO:
-    #  1. Use `stop` parameter with value "\n\n"
-    #  2. Use `stop` parameter with values ["**Embedding Layer**", "**Transformer Blocks**", "**Training**"]
-    #  3. Optional: Set `print_only_content` as False to see the full JSON and what is the `finish_reason`
+USER_QUESTION = "Who is the main character of Wings of liberty?"
+DEPLOYMENT_NAME = "gpt-4o"
+STOP = "Jim Raynor" 
+
+client = DialClient(
+    endpoint=DIAL_ENDPOINT,
+    deployment_name=DEPLOYMENT_NAME,
 )
+conversation = Conversation()
+conversation.add_message(Message(Role.SYSTEM, DEFAULT_SYSTEM_PROMPT))
+conversation.add_message(Message(Role.USER, USER_QUESTION))
 
-# With `stop` parameter we can stop content generation. It can be used for some policies/guardrails. For instance,
-# we are the company with the name `Pear` and we don't want that anybody will see in results that our competitor `Apple`
-# is cool (stop: ["Apple is cool", "Apple top"]).
-# The `finish_reason` will be set as `stop`. So, the users won't know what is the real reason why LLM has stopped generation.
+client.get_completion(
+    messages=conversation.get_messages(),
+    print_request=False,
+    print_only_content=False,  # set False to see full JSON and finish_reason
+    stop=STOP,
+)
